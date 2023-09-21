f472bbf2c6771f9333b167a29764f529882cb231
alencarburitijr <alencar.junior@primaverajd.com.br>
2023-09-21 19:54:01 -0300

    Aula 02 - Variáveis, operadores aritméticos

---
+++ Aula02/Exemplo01.py
@@ -0,0 +1,39 @@
+# Essa parte do código é um comentário
+
+print("Olá mundo!")
+
+# Estou declarando uma variável e atribuindo um valor a ela
+nome = "Alencar Junior"
+
+idade = 33
+
+print("Meu nome é", nome, "e tenho", idade, "anos")
+
+# Operadores aritméticos
+# + - * / % ** //
+valor1 = 10
+valor2 = 18
+
+# Operador de soma para números
+print(valor1 + valor2)
+
+# Realizando operações matemáticas simples  
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+resto = valor1 % valor2
+potencia = valor1 ** valor2
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Resto:", resto)
+print("Potência:", potencia)
+
+# https://agilefingers.com/pt
+
+PI = 3.14
+
+print(PI)
