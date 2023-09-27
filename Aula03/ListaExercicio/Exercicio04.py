95998dfa049056a9d5ccced33141aed570a09399
alencarburitijr <alencar.junior@primaverajd.com.br>
2023-09-27 19:52:46 -0300

    Exercicio 04

--- Aula03/ListaExercicio/Exercicio04.py
+++ Aula03/ListaExercicio/Exercicio04.py
@@ -4,5 +4,7 @@ letra = input("Digite uma letra: ")
 
 if letra.lower() in ('a','e','i','o','u'):
     print('A letra', letra, 'é uma vogal')
+elif letra in ('1','2','3','4','5','6','7','8','9','0'):
+    print('Você digitou um número')
 else:
     print('A letra', letra, 'é uma consoante')
