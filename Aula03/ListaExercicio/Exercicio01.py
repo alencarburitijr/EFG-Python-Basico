2b37fc8ae8702414ef1131afd2d087ee9e44ed74
alencarburitijr <alencar.junior@primaverajd.com.br>
2023-09-27 19:25:54 -0300

    Exercicio

---
+++ Aula03/ListaExercicio/Exercicio01.py
@@ -0,0 +1,18 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+numero1 = input("Digite o primeiro número: ")
+
+numero2 = input("Digite o segundo número: ")
+
+try:
+    numero1 = int(numero1)
+    numero2 = int(numero2)
+    if numero1 > numero2:
+        print("O número", numero1, "é maior que o número", numero2)
+    elif numero1 == numero2:
+        print("Os números são iguais")
+    else:
+        print("O número", numero2, "é maior que o número", numero1)
+except Exception:
+    print("Você não digitou um número válido")
+    exit()
